3bdb9620ee9f5a7c103b6f3ec5f1d18cf717195b
李熹 <114201448+11160233LiXi@users.noreply.github.com>
2024-02-29 16:38:28 +0800

    week02

---
+++ freeglut/week02_glut_sin_cos_circle/main.cpp
@@ -0,0 +1,29 @@
+
+#include <GL/glut.h>
+#include <math.h>
+void circle(float x,float y,float r)
+{
+    glBegin(GL_POLYGON);
+    for(float a=0;a<=3.1415*2;a+=0.1) {
+        glVertex2f(r*cos(a)+x,r*sin(a)+y);
+    }
+    glEnd();
+}
+void display()
+{
+    circle(0.5,0.5,0.3);
+    circle(-0.5,0.5,0.3);
+    circle(0,-0.1,0.6);
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+    glutCreateWindow("week02_color_teapot");
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+}
